2a5046b2a31d016ad25215761bf10efd33273fbf
jamiehatswellhough <jamiehh@live.com>
2018-03-11 19:13:30 +0000

    Running first propel migration from reversed database schema

---
+++ database/migrations/PropelMigration_1520795451.php
@@ -0,0 +1,82 @@
+<?php
+
+use Propel\Generator\Manager\MigrationManager;
+
+/**
+ * Data object containing the SQL and PHP code to migrate the database
+ * up to version 1520795451.
+ * Generated on 2018-03-11 19:10:51 by jamiehatswellhough
+ */
+class PropelMigration_1520795451
+{
+    public $comment = '';
+
+    public function preUp(MigrationManager $manager)
+    {
+        // add the pre-migration code here
+    }
+
+    public function postUp(MigrationManager $manager)
+    {
+        // add the post-migration code here
+    }
+
+    public function preDown(MigrationManager $manager)
+    {
+        // add the pre-migration code here
+    }
+
+    public function postDown(MigrationManager $manager)
+    {
+        // add the post-migration code here
+    }
+
+    /**
+     * Get the SQL statements for the Up migration
+     *
+     * @return array list of the SQL strings to execute for the Up migration
+     *               the keys being the datasources
+     */
+    public function getUpSQL()
+    {
+        return array (
+  'mysql' => '
+# This is a fix for InnoDB in MySQL >= 4.1.x
+# It "suspends judgement" for fkey relationships until are tables are set.
+SET FOREIGN_KEY_CHECKS = 0;
+
+RENAME TABLE `password_resets` TO `password_reset`;
+
+RENAME TABLE `users` TO `user`;
+
+# This restores the fkey checks, after having unset them earlier
+SET FOREIGN_KEY_CHECKS = 1;
+',
+);
+    }
+
+    /**
+     * Get the SQL statements for the Down migration
+     *
+     * @return array list of the SQL strings to execute for the Down migration
+     *               the keys being the datasources
+     */
+    public function getDownSQL()
+    {
+        return array (
+  'mysql' => '
+# This is a fix for InnoDB in MySQL >= 4.1.x
+# It "suspends judgement" for fkey relationships until are tables are set.
+SET FOREIGN_KEY_CHECKS = 0;
+
+RENAME TABLE `password_reset` TO `password_resets`;
+
+RENAME TABLE `user` TO `users`;
+
+# This restores the fkey checks, after having unset them earlier
+SET FOREIGN_KEY_CHECKS = 1;
+',
+);
+    }
+
+}
