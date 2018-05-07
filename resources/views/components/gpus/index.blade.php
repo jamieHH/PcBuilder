85f85c6c2602ab3fadad68d649c7a993e6a4cd10
jamiehatswellhough <jamiehh@live.com>
2018-05-07 22:40:51 +0100

    Adding font awesome icon to the gpus index page

--- resources/views/components/gpus/index.blade.php
+++ resources/views/components/gpus/index.blade.php
@@ -4,7 +4,7 @@
 <div>
     <div class="panel panel-default">
         <div class="panel-heading">
-            <h3 class="panel-title"><b>Graphics Processing Units</b></h3>
+            <h3 class="panel-title"><b><i class="fa fa-microchip"></i>Graphics Processing Units</b></h3>
         </div>
         <div class="panel-body">
             STUFF AND SUCH
