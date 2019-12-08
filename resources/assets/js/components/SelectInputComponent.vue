39e6a13c03734744cc244c43b00ddcd12818ce60
Jamie Hatwell-Hough <jamiehh@live.com>
2019-12-08 18:57:35 +0000

    Adding select input component for select2 elements, and correcting the standard styling to match bootstrap

--- resources/assets/js/components/SelectInputComponent.vue
+++ resources/assets/js/components/SelectInputComponent.vue
@@ -1,8 +1,8 @@
 <template>
     <div class="form-group form-group-lg" v-bind:class="{'has-error' : hasError()}">
         <label v-bind:for="id">{{ label }}</label>
-        <select v-bind:id="id" class="form-control" v-bind:name="name" v-on:input="$emit('input', $event.target.value)" v-bind:value="value">
-            <option v-for="option in options" v-bind:value="option.id">{{ option.name }}</option>
+        <select v-bind:id="id" class="form-control" v-bind:name="name" v-model="value">
+            <option v-for="option in options" v-bind:value="option.id">{{ option.text }}</option>
         </select>
         <span class="help-block" v-if="hasError()">
             <ul v-for="error in fieldErrors">
@@ -15,15 +15,32 @@
 <script>
     export default {
         mounted: function() {
-
+            let vm = this;
+            $('#' + this.id)
+                .select2(this.defaults)
+                .val(this.value)
+                .trigger('change')
+                // emit event on change.
+                .on('change', function () {
+                    vm.$emit('input', this.value);
+                });
         },
         props: [
             'name',
             'label',
             'fieldErrors',
             'options',
+            'placeholder',
             'value'
         ],
+        data: function() {
+            return {
+                defaults: {
+                    placeholder: this.placeholder,
+                    data: this.options
+                }
+            }
+        },
         computed: {
             id: function() {
                 return this.name + '-id';
@@ -33,6 +50,24 @@
             hasError: function() {
                 return (this.fieldErrors !== undefined && this.fieldErrors.length > 0);
             }
+        },
+        watch: {
+            value: function (value) {
+                let vm = this;
+                $('#' + this.id)
+                    .val(value)
+                    .trigger('change')
+                    // emit event on change.
+                    .on('change', function () {
+                        vm.$emit('input', this.value);
+                    });
+            },
+            options: function (options) {
+                $('#' + this.id).empty().select2(this.defaults);
+            }
+        },
+        destroyed: function () {
+            $('#' + this.id).off().select2('destroy')
         }
     }
 </script>
